a0b89b95f193d8228188ba8d1f3be9841c8d3a50
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-12-14 13:46:14 -0300

    fix: events emit

--- src/bubble-chart-events.js
+++ src/bubble-chart-events.js
@@ -1,15 +1,11 @@
 export function onHashtagClick(data)
 {
-    if (window.bubbleChart.hashtagClick)
-    {
-        window.bubbleChart.hashtagClick(data);
-    }
+    const event = new CustomEvent('bubble-chat.on-hash-tag-clicked', { detail: data })
+    document.dispatchEvent(event)
 }
 
 export function onContentClick(data)
 {
-    if (window.bubbleChart.contentClick)
-    {
-        window.bubbleChart.contentClick(data);
-    }
+    const event = new CustomEvent('bubble-chat.on-content-clicked', { detail: data })
+    document.dispatchEvent(event)
 }
